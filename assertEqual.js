17837ba4b1bf0aa6c9fced51c5908a2b7bfbb8ce
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-07-21 23:05:22 +0000

    two siliy emojis are added

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
     console.assert(true, actual === expected)
   } else {
-    console.log(`Assertion failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
     console.assert(true, actual !== expected)
   };
 };
