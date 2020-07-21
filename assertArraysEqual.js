271d3ebd87ba8e5d464cfc8e1e869f8f81e103b0
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-07-21 21:45:30 +0000

    new change saved

---
+++ assertArraysEqual.js
@@ -0,0 +1,26 @@
+
+function eqArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+function assertArraysEqual(arr1, arr2) {
+  if(eqArrays(arr1,arr2)){
+    console.log('The passed-in arrays are absolutely equal to each other')
+  }else{
+    console.log('The passed-in arrays are not equal to each other')
+  }
+}
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
