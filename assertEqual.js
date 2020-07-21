207d9be35c4a3e057112afb2f9b1b37552a4b821
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-07-21 03:08:28 +0000

    this is a function that compares value

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('Assertion Passed' + ":" + actual +  " === " + expected);
+  } else {
+    console.log('Assertion failed' + ":" + actual +  " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 2);
+
+
