845588aecbc4c1957983ce80b799b0ca49de6d42
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-07-21 21:47:52 +0000

    assertion are added

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,10 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`Assertion Passed: ${actual} === ${expected}`);
+    console.assert(true, actual === expected)
   } else {
     console.log(`Assertion failed: ${actual} !== ${expected}`);
+    console.assert(true, actual !== expected)
   };
 };
 
