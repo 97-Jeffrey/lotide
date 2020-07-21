b30d3e0d85d87426e78b40b4b41cc8c577c58765
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-07-21 21:31:46 +0000

    comapre if two arrays are absolutely equal, test with assertEqual

---
+++ eqArrays.js
@@ -0,0 +1,33 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+    console.assert(true, actual === expected)
+  } else {
+    console.log(`Assertion failed: ${actual} !== ${expected}`);
+    console.assert(true, actual !== expected)
+  };
+};
+
+
+function eqArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 4]));
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false) // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true) // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),false);
