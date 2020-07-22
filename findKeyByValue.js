002f10056f6c0efbbbfc5707e47a9a5df0266921
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-07-22 21:17:56 +0000

    function as its name

---
+++ findKeyByValue.js
@@ -0,0 +1,33 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    console.assert(true, actual === expected)
+  } else {
+    console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
+    console.assert(true, actual !== expected)
+  };
+};
+
+
+const findKeyByValue = function (givenObject, givenValue) {
+
+  let array = Object.keys(givenObject);
+  let theKey;
+  for (let each of array) {
+    if (givenObject[each] === givenValue) {
+      theKey = each;
+    }
+  }
+  return theKey;
+}
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse"));
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
