a39548173c9d91565197d017cc5437d9a30d013b
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-07-22 05:38:51 +0000

    the tail function is modified

--- tail.js
+++ tail.js
@@ -1,9 +1,11 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    console.assert(true, actual === expected)
   } else {
-    console.log(`Assertion failed: ${actual} !== ${expected}`);
-  }
+    console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
+    console.assert(true, actual !== expected)
+  };
 };
 
 const tail = function(arr) {
@@ -11,13 +13,15 @@ const tail = function(arr) {
   for (let i = 0; i < arr.length; i++) {
     arr2.push(arr[i]);
   }
-  arr2[0] = undefined;
+  // arr2[0] = undefined;
+  arr2.shift();
   return arr2;
 };
 
 
 
+
  
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
+console.log(tail(words)); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
