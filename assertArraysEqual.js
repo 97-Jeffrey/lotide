81572fc88aea067868c70fce0ab602ad49d9253f
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-07-23 20:25:56 +0000

    some subtle changes about return statemnet

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -14,9 +14,11 @@ function eqArrays(arr1, arr2) {
 
 function assertArraysEqual(arr1, arr2) {
   if(eqArrays(arr1,arr2)){
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`)
+    // console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+    return `✅✅✅ Assertion Passed: ${arr1} === ${arr2}`;
   }else{
-    console.log(`🛑🛑🛑 Assertion failed: ${arr1} !== ${arr2}`)
+    // console.log(`🛑🛑🛑 Assertion failed: ${arr1} !== ${arr2}`);
+    return `🛑🛑🛑 Assertion failed: ${arr1} !== ${arr2}`;
   }
 }
 
