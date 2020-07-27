1a720ae171fcee213b13bfc7f9b4aa94d1f5cddb
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-07-27 20:09:07 +0000

    refractor the test code with mocha describe and it

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,19 @@
 const head = require('../head').head;
-const assertEqual = require('../head').assertEqual;
+const assert = require('chai').assert;
+// const assertEqual = require('../head').assertEqual;
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head([]), "Hello");
+
+describe('#head',()=>{
+  it('return 1 for [1,2,3', ()=>{
+    assert.strictEqual(head([1,2,3]),1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it('return 5 for [5,6,7]',()=>{
+    assert.strictEqual(head([5,6,7]),5);
+  });
+  it('return "hello" from ["hello"]', ()=>{
+    assert.strictEqual(head(['hello']),'hello');
+  })
+});
