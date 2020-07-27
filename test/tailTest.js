6d051046f48a1b36c3066c396de350c3256df668
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-07-27 20:40:14 +0000

    refractor test code by describe and it

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,18 @@
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+// const assertEqual = require('../assertEqual');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+
+describe('#tail',()=>{
+  it('length of word should equal 3 when ["Yo Yo", "Lighthouse", "Labs"]',()=>{
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    assert.equal(words.length, 3);
+  });
+
+  it('length of words should equal 4 ["what", "is", "going", "on"]', ()=>{
+    const words = ["what", "is", "going", "on"];
+    tail(words);
+    assert.equal(words.length,4);
+  })
+});
