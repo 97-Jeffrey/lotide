ab945aaa9716433693a9628088a563309d8f9db8
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-07-27 23:00:50 +0000

    final refractor

--- letterPositions.js
+++ letterPositions.js
@@ -1,27 +1,3 @@
-const  assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    // console.log('The passed-in arrays are absolutely equal to each other')
-    return '✅✅✅ The passed-in arrays are absolutely equal to each other';
-  } else {
-    // console.log('The passed-in arrays are not equal to each other')
-    return '🛑🛑🛑 The passed-in arrays are not equal to each other';
-  }
-};
-
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-
 
 const letterPositions = function(sentence) {
   const results = {};
@@ -40,6 +16,10 @@ const letterPositions = function(sentence) {
   return results;
 };
 
-console.log(letterPositions('lighthouse in the house'));
+module.exports = letterPositions;
 
-console.log(assertArraysEqual(letterPositions("lighthouse in the house").h, [3, 5, 15, 18]));
+
+
+// console.log(letterPositions('lighthouse in the house'));
+
+// console.log(assertArraysEqual(letterPositions("lighthouse in the house").h, [3, 5, 15, 18]));
