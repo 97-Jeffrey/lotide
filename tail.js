15b07fde6af230e5cef026709bb787137416a700
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-07-27 07:17:53 +0000

    require and exports are added into the function, test code are removed

--- tail.js
+++ tail.js
@@ -1,12 +1,5 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    console.assert(true, actual === expected)
-  } else {
-    console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
-    console.assert(true, actual !== expected)
-  };
-};
+
+const assertEqual = require('./assertEqual');
 
 const tail = function (arr) {
   let arr1 = arr.slice(1);
@@ -15,8 +8,4 @@ const tail = function (arr) {
 
 
 
-
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
