05853008b028674f7a4d574010c12357e7a05a8b
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-07-27 23:01:18 +0000

    final refractor

--- map.js
+++ map.js
@@ -1,32 +1,8 @@
-const words = ["ground", "control", "to", "major", "tom"];
 
-
-function eqArrays(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-
-function assertArraysEqual(arr1, arr2) {
-  if(eqArrays(arr1,arr2)){
-    //console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`)
-    return `✅✅✅ Assertion Passed: ${arr1} === ${arr2}`
-  }else{
-    // console.log(`🛑🛑🛑 Assertion failed: ${arr1} !== ${arr2}`)
-    return `🛑🛑🛑 Assertion failed: ${arr1} !== ${arr2}`
-  }
-}
 
 
 // this is the callback function:
-const getLength = function(item){
+const getLength = function (item) {
   return item[0];
 }
 
@@ -39,11 +15,11 @@ const map = function (array, callback) {
   return results;
 }
 
+module.exports = {
+  getLength: getLength,
+  map: map
+}
+
 //test with assertArrayEqual:
-console.log(assertArraysEqual(map(words, getLength),['g','c','t','m','t']));
-
-
-
-
-
-console.log(map(words, getLength));
+// console.log(assertArraysEqual(map(words, getLength),['g','c','t','m','t']));
+// console.log(map(words, getLength));
