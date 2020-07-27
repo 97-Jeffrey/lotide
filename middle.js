91ad9a96d13b0d7f5ff19a5a77d55ec8dfcd3748
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-07-27 23:01:50 +0000

    final refractor

--- middle.js
+++ middle.js
@@ -1,25 +1,4 @@
-// function eqArrays(arr1, arr2) {
-//   if (arr1.length !== arr2.length) {
-//     return false;
-//   }
-//   for (let i = 0; i < arr1.length; i++) {
-//     if (arr1[i] !== arr2[i]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// }
 
-
-// function assertArraysEqual(arr1, arr2) {
-//   if (eqArrays(arr1, arr2)) {
-//     //console.log('The passed-in arrays are absolutely equal to each other');
-//     return 'The passed-in arrays are absolutely equal to each other';
-//   } else {
-//     //console.log('The passed-in arrays are not equal to each other')
-//     return  'The passed-in arrays are not equal to each other'
-//   }
-// }
 
 
 function middle(array) {
