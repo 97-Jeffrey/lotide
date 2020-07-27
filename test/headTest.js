dcfd55b4c8ef704b8aaecfbd45f4f8304782b921
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-07-27 19:24:12 +0000

    require two things from same js file

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,5 @@
-const head = require('../head');
-const assertEqual = require('../assertEqual');
+const head = require('../head').head;
+const assertEqual = require('../head').assertEqual;
 
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head([]), "Hello");
