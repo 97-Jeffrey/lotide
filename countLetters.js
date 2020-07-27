93a89959949bfda63c2c04c62a973cf35d8c3521
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-07-27 22:53:44 +0000

    final refactor

--- countLetters.js
+++ countLetters.js
@@ -1,16 +1,3 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    // console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    console.assert(true, actual === expected);
-    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
-  } else {
-    // console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
-    console.assert(true, actual !== expected);
-    return `🛑🛑🛑Assertion failed: ${actual} !== ${expected}`
-
-  };
-};
-
 
 const countLetters = function (str) {
   let output = {};
@@ -28,7 +15,10 @@ const countLetters = function (str) {
   return output;
 }
 
-let result = countLetters('what is going on');
- console.log(result);
+module.exports = countLetters;
+
+
+// let result = countLetters('what is going on');
+//  console.log(result);
 //  console.log(assertEqual(result['a'],3))
 //  console.log(assertEqual(result['b'],4))
